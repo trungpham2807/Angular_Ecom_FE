c6459d62f932740f855fe5dcc8ab863163b03f96
Fadi Nouh <fadinoh@gmail.com>
2021-04-24 17:26:34 +0200

    changes

---
+++ apps/admin/src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '@bluebits/orders';
+import { ProductsService } from '@bluebits/products';
+import { UsersService } from '@bluebits/users';
+import { combineLatest } from 'rxjs';
+
+@Component({
+  selector: 'admin-dashboard',
+  templateUrl: './dashboard.component.html'
+})
+export class DashboardComponent implements OnInit {
+  statistics = [];
+  constructor(
+    private userService: UsersService,
+    private productService: ProductsService,
+    private ordersService: OrdersService
+  ) {}
+
+  ngOnInit(): void {
+    combineLatest([
+      this.ordersService.getOrdersCount(),
+      this.productService.getProductsCount(),
+      this.userService.getUsersCount(),
+      this.ordersService.getTotalSales()
+    ]).subscribe((values) => {
+      this.statistics = values;
+    });
+  }
+}
---
+++ libs/products/src/lib/products.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrdersModule } from '@bluebits/orders';
+
+@NgModule({
+  imports: [CommonModule, OrdersModule],
+})
+export class ProductsModule {}
